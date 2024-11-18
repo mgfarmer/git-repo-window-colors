e401dee9eb0dff43733bca384ebc33903245197d
Kevin Mills <kmills@mips.com>
2024-11-17 17:36:01 -0800

    updates!

--- README.md
+++ README.md
@@ -4,46 +4,42 @@ Color your VSCode Windows based on which git repo you are working in.
 
 ## What it does
 
-This extension gives your VSCode window a unique color based on the repository name of the git repository opened as your worksapce.  It does this by immediately writing three colors to the following settings in `.vscode/settings.json`:
+This extension allow you to give your VSCode window a custom color based on the repository name of the git repository opened as your worksapce.  It does this by immediately writing the following settings in `.vscode/settings.json`:
 
 ```javascript
   "workbench.colorCustomizations": {
-    "activityBar.background": "#13332E",
-    "titleBar.activeBackground": "#19423B",
-    "titleBar.activeForeground": "#F6FBFB"
+    "activityBar.background": "#XXXXXX",
+    "activityBar.foreground": "#XXXXXX",
+    "titleBar.activeBackground": "#XXXXXX",
+    "titleBar.activeForeground": "#XXXXXX",
+    "titleBar.inactiveBackground": "#XXXXXX",
+    "titleBar.inactiveForeground": "#XXXXXX"
   }
 ```
 
-## Usage with Git
-
-To avoid checking `.vscode/settings.json` in to your remote repository without modifying `.gitignore`, you can either:
-
-1. **locally:** add `.vscode/settings.json` to your project's `.git/info/exclude` file
-
-    _or_
-
-2.  **globally:** create and use a global `.gitignore_global` file like so:
-
-    ```git config --global core.excludesfile ~/.gitignore_global```
+If you are like me and have multiple vscode windows opened all the time, working on different repositories, you know it can be hard to differentiate the windows visually.  By assigning custom colors to the title and activity bars, this plugin aims to alleviate this problem.  This also helps when viewing the thumbnail previews displayed by most docks (Windows Taskbar, Ubuntu Dock, etc...)
 
 ## Usage
 
-Any custom colors you may have defined previously in a workspace settings.json will be overwritten by this extension.
+Any custom color settings managed by this plugin that you may have defined previously in a workspace settings.json will be overwritten by this extension.
 
-To configure this extenstion, open the Settings UI, head to the extension settings and update the `Window Colors: Configuration.`  This setting is a json array of objects of this form:
+To configure this extenstion, open the Settings UI and update the `Window Colors: Repo Configuration List.`  
 
-```json
-[ 
-  { "name": "repo-name-1", "color" : "blue" },
-  { "name": "repo-name-2", "color" : "green" } 
-]
-```
+This setting is a configurable list of string entries.  Each string has this format schema:
 
-The color value can be any color string code recognized by javascript Color().
+`<repo-qualifier>[/<default-branch>] : <primary-color>[/<branch-color>]`
+
+Where:
+- `<repo-qualifier>`: This is required. The repository qualifier is a simple string. If this string is found in the repository URL as given by `git config --get remote.origin.url`, then a match is made.  The first match found  will be used.
+- `<default-branch>`: This is optional. and represents the default GitHub branch for the repository. If the `default-branch` is specified then the activity bar will be shown in a different color when you are not working on the default branch. This other color is specified either by the `branch-color` setting, if provided, or automatically determined using the `Automatic Branch Indicator Color Knob` in settings. You can invert the branch coloring logic in the settings as well.
+- `<primary-color>`: This is required.  The primary color is the color associated with the repository.  This color will be used to color the window title bar and activity bar (when not using branch colors).
+- `<branch-color>`: This is a color that will be used for the activity bar when working on a non-default branch, or on the default branch, depending on the `Invert Branch Color Logic` setting.
+
+The color value can be any color string code recognized by javascript `Color()`.  
 
 When editing the settings, the VSCode window will respond immediately to your edits.  Any errors (broken json, unknown color codes, etc...) are reported via VSCode notifiaction messages.
 
-If your workspace folder is a git repo, then the repository name is extracted using `git config --get remote.origin.url`. If the 'name' value is found in the output of that command (simple sting search), then a match is made and the 'color' from that match is applied to the window.  The first match found is used.
+If your workspace folder is not a git repo, then no coloring will happen.
 
 If you use VSCode Settings Sync then these color configuraiton will apply everywhere you use vscode.
 
@@ -55,5 +51,5 @@ When opening new VSCode windows, you might see the relevant theme colors change 
 
 ## Credits
 
-This projects was inspired by and forked from https://github.com/stuartcrobinson/unique-window-colors
+This projects was inspired by and forked from https://github.com/stuartcrobinson/unique-window-colors.
 
--- src/extension.ts
+++ src/extension.ts
@@ -6,31 +6,59 @@ import { execSync } from "child_process";
 import * as vscode from "vscode";
 import { ColorThemeKind, ExtensionContext, window, workspace } from "vscode";
 
+let currentBranch: undefined | string = undefined;
+
 function doit() {
   // windowColors.configuration
-  const json = workspace
+  const obj = workspace
     .getConfiguration("windowColors")
-    .get<string>("configuration");
-  if (json === undefined || json == "") {
-    // No configuration found
-    vscode.window.showInformationMessage(
-      "No custom git repo color settings defined, please set some up in the setting."
-    );
+    .get<object>("repoConfigurationList");
+
+  const doColorInactiveTitlebar = workspace
+    .getConfiguration("windowColors")
+    .get<boolean>("colorInactiveTitlebar");
+
+  const doColorActiveTitlebar = workspace
+    .getConfiguration("windowColors")
+    .get<boolean>("colorActiveTitlebar");
+
+  const invertBranchColorLogic = workspace
+    .getConfiguration("windowColors")
+    .get<boolean>("invertBranchColorLogic");
+
+  if (obj === undefined || Object.keys(obj).length === 0) {
     return;
   }
 
-  let customColors = undefined;
-  try {
-    customColors = JSON.parse(json);
-  } catch (error) {
-    const e = error as Error;
-    vscode.window.showErrorMessage(
-      "Configuration json is invalid: " + e.message
-    );
-    return;
+  let hueRotation = workspace
+    .getConfiguration("windowColors")
+    .get<number>("automaticBranchIndicatorColorKnob");
+  if (hueRotation === undefined) {
+    hueRotation = 60;
   }
 
-  console.group(customColors);
+  let activityBarColorKnob = workspace
+    .getConfiguration("windowColors")
+    .get<number>("activityBarColorKnob");
+  if (activityBarColorKnob === undefined) {
+    activityBarColorKnob = 3;
+  }
+  activityBarColorKnob = activityBarColorKnob / 10;
+
+  let json = JSON.parse(JSON.stringify(obj));
+
+  for (const key in json) {
+    const val = json[key] as string;
+    console.log(val.replaceAll(" ", ""));
+  }
+
+  // if (json === undefined || json == "") {
+  //   // No configuration found
+  //   vscode.window.showInformationMessage(
+  //     "No custom git repo color settings defined, please set some up in the setting."
+  //   );
+  //   return;
+  // }
 
   /** retain initial unrelated colorCustomizations*/
   const cc = JSON.parse(
@@ -39,24 +67,17 @@ function doit() {
     )
   );
 
-  // const configFile = path.join(os.homedir(), ".config", "vscode.git.colors.json")
-  // if (!fs.existsSync(configFile)) {
-  //   // Accept native color behavior
-  //   return;
-  // }
-  // const customColors = fs.readJSONSync(configFile)
-
   if (workspace.workspaceFolders === undefined) {
     return;
   }
 
   let workspaceRoot: string = workspace.workspaceFolders[0].uri.fsPath;
-  process.chdir(workspaceRoot);
 
   let repoName = "";
   try {
     repoName = execSync("git config --get remote.origin.url", {
       encoding: "utf-8",
+      cwd: workspaceRoot,
     }).trim();
   } catch (error) {
     console.error("Error:", error);
@@ -66,83 +87,156 @@ function doit() {
     return;
   }
 
-  let sideBarColor = undefined;
+  let repoColor = undefined;
+  let branchColor = undefined;
+  let defBranch = undefined;
 
-  for (const item of customColors) {
-    if (repoName.includes(item["name"])) {
+  for (const item in json) {
+    defBranch = undefined;
+    const parts = json[item].split(":");
+    if (parts.length !== 2) {
+      // Invalid entry
+      continue;
+    }
+    const repoParts = parts[0].split("/");
+    const repo = repoParts[0].trim();
+    if (repoParts.length > 1) {
+      defBranch = repoParts[1].trim();
+    }
+
+    const colorParts = parts[1].split("/");
+    const rColor = colorParts[0].trim();
+    let bColor = undefined;
+    if (colorParts.length > 1) {
+      bColor = colorParts[1].trim();
+    }
+
+    if (repoName.includes(repo)) {
       try {
-        sideBarColor = Color(item["color"]);
+        repoColor = Color(rColor);
       } catch (error) {
-        sideBarColor = undefined;
+        repoColor = undefined;
         vscode.window.showInformationMessage(
-          "Could not parse color: " + item["color"]
+          "Could not parse repo color: " + rColor
         );
       }
+      if (defBranch !== undefined) {
+        try {
+          if (bColor !== undefined) {
+            branchColor = Color(bColor);
+          }
+        } catch (error) {
+          branchColor = undefined;
+          vscode.window.showInformationMessage(
+            "Could not parse branch color: " + bColor
+          );
+        }
+      }
+
+      break;
     }
   }
 
-  if (sideBarColor === undefined) {
+  if (repoColor === undefined) {
     return;
+  }
+
+  if (defBranch !== undefined) {
+    if (
+      (!invertBranchColorLogic && currentBranch != defBranch) ||
+      (invertBranchColorLogic && currentBranch === defBranch)
+    ) {
+      // Not on the default branch
+      if (branchColor === undefined) {
+        // No color specified, use modified repo color
+        branchColor = repoColor.rotate(hueRotation);
+      }
+    } else {
+      // On the default branch
+      branchColor = repoColor;
+    }
+  } else {
+    branchColor = repoColor;
   }
 
   let titleBarTextColor: Color = Color("#ffffff");
   let titleBarColor: Color = Color("#ffffff");
-
-  const sideBarColor_dark = getColorWithLuminosity(sideBarColor, 0.02, 0.027);
-  const titleBarTextColor_dark = getColorWithLuminosity(
-    sideBarColor_dark,
-    0.95,
-    1
-  );
-  const titleBarColor_dark = sideBarColor_dark.lighten(0.4);
-
-  const sideBarColor_light = getColorWithLuminosity(sideBarColor, 0.45, 0.55);
-  const titleBarTextColor_light = getColorWithLuminosity(
-    sideBarColor_light,
-    0,
-    0.01
-  );
-  const titleBarColor_light = sideBarColor_light.lighten(0.1);
+  let titleInactiveBarColor: Color = Color("#ffffff");
+  //let titleBarBorderColor: Color = Color("red");
+  let sideBarColor: Color = Color("#ffffff");
 
   const theme: ColorThemeKind = window.activeColorTheme.kind;
 
   if (theme === ColorThemeKind.Dark) {
-    sideBarColor = sideBarColor_dark;
-    titleBarTextColor = titleBarTextColor_dark;
-    titleBarColor = titleBarColor_dark;
+    sideBarColor = doColorActiveTitlebar
+      ? branchColor.lighten(activityBarColorKnob)
+      : repoColor;
+    // sideBarColor = doColorActiveTitlebar
+    //   ? getColorWithLuminosity(
+    //       branchColor,
+    //       activityBarColorKnob,
+    //       activityBarColorKnob + 0.01
+    //     )
+    //   : repoColor;
+    titleBarTextColor = getColorWithLuminosity(repoColor, 0.95, 1);
+    titleBarColor = repoColor;
+    titleInactiveBarColor = titleBarColor.darken(0.25);
   } else if (theme === ColorThemeKind.Light) {
-    sideBarColor = sideBarColor_light;
-    titleBarTextColor = titleBarTextColor_light;
-    titleBarColor = titleBarColor_light;
+    sideBarColor = doColorActiveTitlebar
+      ? branchColor.darken(activityBarColorKnob)
+      : repoColor;
+    // sideBarColor = doColorActiveTitlebar
+    //   ? getColorWithLuminosity(
+    //       branchColor,
+    //       activityBarColorKnob,
+    //       activityBarColorKnob + 0.01
+    //     )
+    //   : repoColor;
+    if (repoColor.isDark()) {
+      titleBarTextColor = getColorWithLuminosity(repoColor, 0.95, 1);
+    } else {
+      titleBarTextColor = getColorWithLuminosity(repoColor, 0, 0.01);
+    }
+    titleBarColor = repoColor;
+    titleInactiveBarColor = titleBarColor.lighten(0.15);
   }
 
-  const doRemoveColors = false;
+  const newColors = {
+    //"titleBar.border": titleBarBorderColor.hex(),
+    "activityBar.background": sideBarColor.hex(),
+    "activityBar.foreground": titleBarTextColor.hex(),
+    "titleBar.activeBackground": doColorActiveTitlebar
+      ? titleBarColor.hex()
+      : undefined,
+    "titleBar.activeForeground": doColorActiveTitlebar
+      ? titleBarTextColor.hex()
+      : undefined,
+    "titleBar.inactiveBackground": doColorInactiveTitlebar
+      ? titleInactiveBarColor.hex()
+      : undefined,
+    "titleBar.inactiveForeground": doColorInactiveTitlebar
+      ? titleBarTextColor.hex()
+      : undefined,
+  };
+  workspace
+    .getConfiguration("workbench")
+    .update("colorCustomizations", { ...cc, ...newColors }, false);
+}
 
-  let doUpdateColors = true;
-
-  // if (
-  //   cc &&
-  //   (cc["activityBar.background"] ||
-  //     cc["titleBar.activeBackground"] ||
-  //     cc["titleBar.activeForeground"])
-  // ) {
-  //   //don't overwrite
-  //   doUpdateColors = false;
-  // }
-
-  if (doUpdateColors || doRemoveColors) {
-    const newColors = {
-      "activityBar.background": doRemoveColors ? undefined : sideBarColor.hex(),
-      "titleBar.activeBackground": doRemoveColors
-        ? undefined
-        : titleBarColor.hex(),
-      "titleBar.activeForeground": doRemoveColors
-        ? undefined
-        : titleBarTextColor.hex(),
-    };
-    workspace
-      .getConfiguration("workbench")
-      .update("colorCustomizations", { ...cc, ...newColors }, false);
+function getCurrentBranch(): string {
+  try {
+    if (workspace.workspaceFolders === undefined) {
+      return "";
+    }
+    let workspaceRoot: string = workspace.workspaceFolders[0].uri.fsPath;
+    const branch = execSync("git rev-parse --abbrev-ref HEAD", {
+      encoding: "utf-8",
+      cwd: workspaceRoot,
+    }).trim();
+    return branch;
+  } catch (error) {
+    console.error("Error:", error);
+    return "";
   }
 }
 
@@ -153,11 +247,61 @@ export function activate(context: ExtensionContext) {
 
   context.subscriptions.push(
     vscode.workspace.onDidChangeConfiguration(async (e) => {
-      if (e.affectsConfiguration("windowColors.configuration")) {
+      if (
+        e.affectsConfiguration("windowColors") ||
+        // e.affectsConfiguration("windowColors.colorInactiveTitlebar") ||
+        // e.affectsConfiguration("windowColors.colorActiveTitlebar") ||
+        // e.affectsConfiguration("windowColors.activityBarColorKnob") ||
+        // e.affectsConfiguration("windowColors.invertBranchColorLogic") ||
+        // e.affectsConfiguration("windowColors.automaticBranchIndicatorColorKnob") ||
+        e.affectsConfiguration("workbench.colorTheme")
+        //
+      ) {
         doit();
       }
     })
   );
+
+  currentBranch = getCurrentBranch();
+
+  setInterval(function () {
+    let branch = "";
+    try {
+      if (workspace.workspaceFolders === undefined) {
+        return;
+      }
+      branch = getCurrentBranch();
+      if (currentBranch != branch) {
+        currentBranch = branch;
+        console.log("change to branch: " + branch);
+        doit();
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      return;
+    }
+  }, 2000);
+
+  // const gitExtension = extensions.getExtension("vscode.git")!.exports;
+  // //const gitBaseExtension = extensions.getExtension("vscode.git-base")!.exports;
+
+  // const git = gitExtension.getAPI(1);
+  // //const gitbase = gitBaseExtension.getAPI(1);
+
+  // git.onDidChangeState(() => {
+  //   console.log("repo state change!");
+  //   if (git.repositories.length > 0) {
+  //     try {
+  //       const repo = git.repositories[0];
+  //       console.log(repo);
+  //       repo.state.onDidChange(() => {
+  //         console.log("repo!");
+  //       });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  // });
 
   doit();
 }
@@ -168,12 +312,17 @@ const getColorWithLuminosity = (
   max: number
 ): Color => {
   let c: Color = Color(color.hex());
-
-  while (c.luminosity() > max) {
+  let iter = 0;
+  while (c.luminosity() > max && iter < 10000) {
     c = c.darken(0.01);
+    iter++;
   }
-  while (c.luminosity() < min) {
+  console.log(iter);
+  iter = 0;
+  while (c.luminosity() < min && iter < 10000) {
     c = c.lighten(0.01);
+    iter++;
   }
+  console.log(iter);
   return c;
 };
